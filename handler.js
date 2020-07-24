fa669da55ee994c3f1ba6d5ac0c1b0d05d28021d
Kenny Yeo <kenny@yeoyou.net>
2020-07-24 23:09:29 +1000

    add logs

--- handler.js
+++ handler.js
@@ -7,9 +7,11 @@ const app = require('./app');
 const awsServerlessExpressServer = awsServerlessExpress.createServer(app);
 
 module.exports.serverlessExpress = async (event, context) =>
+  console.log('serverlessExpress:', event, context) ||
   awsServerlessExpress.proxy(awsServerlessExpressServer, event, context);
 
 const serverlessHttpServer = serverlessHttp(app);
 
 module.exports.serverlessHttp = async (event, context) =>
+  console.log('serverlessHttp:', event, context) ||
   serverlessHttpServer(event, context);
